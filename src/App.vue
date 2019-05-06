ce5b71523c10d08044003c670f81c83eed8d69ed
xiongtianming <xiongtianming@yat-hp.net>
2019-05-06 18:42:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import MHeader from '@/components/m-header/m-header.vue';
+  export default {
+    components: {
+      MHeader,
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "./common/stylus/variable.styl"
+
+  #app
+    color: $color-theme
+</style>
